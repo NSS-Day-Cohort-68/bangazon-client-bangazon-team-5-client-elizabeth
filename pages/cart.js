b8b6fac59418ab7c3430b3f8f63c21ca4330c054
Claire Hanson <clairecatohanson@gmail.com>
2024-04-01 11:54:38 -0500

    updated fetcher 404 error handling, fixed completeCurrentOrder()

--- pages/cart.js
+++ pages/cart.js
@@ -1,39 +1,48 @@
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import CardLayout from '../components/card-layout'
-import Layout from '../components/layout'
-import Navbar from '../components/navbar'
-import CartDetail from '../components/order/detail'
-import CompleteFormModal from '../components/order/form-modal'
-import { completeCurrentOrder, getCart } from '../data/orders'
-import { getPaymentTypes } from '../data/payment-types'
-import { removeProductFromOrder } from '../data/products'
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import CardLayout from "../components/card-layout"
+import Layout from "../components/layout"
+import Navbar from "../components/navbar"
+import CartDetail from "../components/order/detail"
+import CompleteFormModal from "../components/order/form-modal"
+import { completeCurrentOrder, getCart } from "../data/orders"
+import { getPaymentTypes } from "../data/payment-types"
+import { removeProductFromOrder } from "../data/products"
+import { useAppContext } from "../context/state"
 
 export default function Cart() {
   const [cart, setCart] = useState({})
   const [paymentTypes, setPaymentTypes] = useState([])
   const [showCompleteForm, setShowCompleteForm] = useState(false)
   const router = useRouter()
+  const { profile } = useAppContext()
 
   const refresh = () => {
-    getCart().then(cartData => {
+    getCart().then((cartData) => {
       if (cartData) {
         setCart(cartData)
+      } else {
+        setCart({})
       }
     })
   }
 
   useEffect(() => {
     refresh()
-    getPaymentTypes().then(paymentData => {
-      if (paymentData) {
-        setPaymentTypes(paymentData)
-      }
-    })
-  }, [])
+    if (profile && profile.id) {
+      getPaymentTypes(profile.id).then((paymentData) => {
+        if (paymentData) {
+          setPaymentTypes(paymentData)
+        }
+      })
+    }
+  }, [profile])
 
   const completeOrder = (paymentTypeId) => {
-    completeCurrentOrder(cart.id, paymentTypeId).then(() => router.push('/my-orders'))
+    completeCurrentOrder(cart.id, paymentTypeId).then(() => {
+      setCart({})
+      router.push("/my-orders")
+    })
   }
 
   const removeProduct = (productId) => {
@@ -51,7 +60,12 @@ export default function Cart() {
       <CardLayout title="Your Current Order">
         <CartDetail cart={cart} removeProduct={removeProduct} />
         <>
-          <a className="card-footer-item" onClick={() => setShowCompleteForm(true)}>Complete Order</a>
+          <a
+            className="card-footer-item"
+            onClick={() => setShowCompleteForm(true)}
+          >
+            Complete Order
+          </a>
           <a className="card-footer-item">Delete Order</a>
         </>
       </CardLayout>
