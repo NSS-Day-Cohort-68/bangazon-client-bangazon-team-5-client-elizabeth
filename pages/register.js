ad687c31e811f7bc1f90a15dfa4ab6d7b46c26d1
Claire Hanson <clairecatohanson@gmail.com>
2024-04-22 09:44:39 -0500

    added input fields to registration form

--- pages/register.js
+++ pages/register.js
@@ -1,19 +1,22 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useRef } from 'react'
-import { Input } from '../components/form-elements'
-import Layout from '../components/layout'
-import Navbar from '../components/navbar'
-import { useAppContext } from '../context/state'
-import { register } from '../data/auth'
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useRef } from "react"
+import { Input } from "../components/form-elements"
+import Layout from "../components/layout"
+import Navbar from "../components/navbar"
+import { useAppContext } from "../context/state"
+import { register } from "../data/auth"
 
 export default function Register() {
-  const {setToken} = useAppContext()
+  const { setToken } = useAppContext()
 
-  const firstName = useRef('')
-  const lastName = useRef('')
-  const username = useRef('')
-  const password = useRef('')
+  const firstName = useRef("")
+  const lastName = useRef("")
+  const username = useRef("")
+  const password = useRef("")
+  const email = useRef("")
+  const phoneNumber = useRef("")
+  const address = useRef("")
   const router = useRouter()
 
   const submit = (e) => {
@@ -23,13 +26,16 @@ export default function Register() {
       username: username.current.value,
       password: password.current.value,
       first_name: firstName.current.value,
-      last_name: lastName.current.value
+      last_name: lastName.current.value,
+      email: email.current.value,
+      phone_number: phoneNumber.current.value,
+      address: address.current.value,
     }
 
     register(user).then((res) => {
       if (res.token) {
         setToken(res.token)
-        router.push('/')
+        router.push("/")
       }
     })
   }
@@ -45,29 +51,33 @@ export default function Register() {
             type="text"
             label="First Name"
           />
-          <Input
-            id="lastName"
-            refEl={lastName}
-            type="text"
-            label="Last Name"
-          />
-
-          <Input
-            id="username"
-            refEl={username}
-            type="text"
-            label="Username"
-          />
+          <Input id="lastName" refEl={lastName} type="text" label="Last Name" />
+          <Input id="email" refEl={email} type="text" label="Email" />
+          <Input id="username" refEl={username} type="text" label="Username" />
           <Input
             id="password"
             refEl={password}
             type="password"
             label="Password"
           />
+          <Input
+            id="phoneNumber"
+            refEl={phoneNumber}
+            type="tel"
+            label="Phone Number"
+          />
+          <Input
+            id="address"
+            refEl={address}
+            type="text"
+            label="Street Address"
+          />
 
           <div className="field is-grouped">
             <div className="control">
-              <button className="button is-link" onClick={submit}>Submit</button>
+              <button className="button is-link" onClick={submit}>
+                Submit
+              </button>
             </div>
             <div className="control">
               <Link href="/login">
